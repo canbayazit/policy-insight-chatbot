8355e62138c3c5575f882745e92e62564d385377
Can Bayazit <rovahelsing@gmail.com>
2025-10-23 21:14:02 +0300

    added navigation link on logo to navigate home page

--- frontend/policy-insight-chatbot-ui/src/components/Header/Header.tsx
+++ frontend/policy-insight-chatbot-ui/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <header className="h-16 flex items-center justify-between px-4 md:px-6 border-b border-border bg-white/90 dark:bg-gray-800/60 backdrop-blur">
       {/* Sol: Logo + Ürün adı */}
-      <div className="flex items-center gap-3 min-w-0">
+      <Link to="/" className="flex items-center gap-3 min-w-0">
         <div className="size-8 text-[--color-primary] shrink-0">
           <svg viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden>
             <path d="M13.8261 17.4264C16.7203 18.1174 20.2244 18.5217 24 18.5217C27.7756 18.5217 31.2797 18.1174 34.1739 17.4264C36.9144 16.7722 39.9967 15.2331 41.3563 14.1648L24.8486 40.6391C24.4571 41.267 23.5429 41.267 23.1514 40.6391L6.64374 14.1648C8.00331 15.2331 11.0856 16.7722 13.8261 17.4264Z"></path>
@@ -18,7 +19,7 @@ const Header: React.FC = () => {
         <h1 className="text-[var(--color-primary)] dark:text-white font-bold truncate">
           Policy Insight
         </h1>
-      </div>
+      </Link>
       <div className="flex items-center gap-1">
         <ThemeToggle />
       </div>
