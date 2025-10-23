dd406ca043764646704b0b572045f964c0070879
Can Bayazit <rovahelsing@gmail.com>
2025-10-23 21:11:43 +0300

    added routing structure with react router dom

--- frontend/policy-insight-chatbot-ui/src/App.tsx
+++ frontend/policy-insight-chatbot-ui/src/App.tsx
@@ -1,23 +1,9 @@
-import React, { useState } from "react";
-import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Chat from "./components/Chat/Chat";
-import FileUploader from "./components/FileUploader/FileUploader";
+import React from "react";
+import RouteLayout from "./routes/RouteLayout";
 
 const App = () => {
-  const hasPdf = true;
   return (
-    <div className="h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
-      <Header />
-      <div className="flex flex-1 min-h-0 overflow-hidden">
-        {hasPdf && <Sidebar />}
-        <main className="flex-1 flex flex-col min-w-0">
-          <div className="flex-1 min-h-0 overflow-y-auto">
-            {hasPdf ? <Chat/> : <FileUploader />}
-          </div>
-        </main>
-      </div>
-    </div>
+    <RouteLayout/>
   );
 };
 
---
+++ frontend/policy-insight-chatbot-ui/src/routes/RouteLayout.tsx
@@ -0,0 +1,32 @@
+import React, { type JSX } from "react";
+import Header from "../components/Header/Header";
+import FileUploader from "../components/FileUploader/FileUploader";
+import { Routes, Route } from "react-router";
+import ChatLayout from "../components/ChatLayout/ChatLayout";
+
+interface IRoute {
+  path: string;
+  element: JSX.Element;
+}
+
+const routes: IRoute[] = [
+  { path: "/", element: <FileUploader /> },
+  { path: "/chat", element: <ChatLayout /> },
+];
+
+const RouteLayout = () => {
+  return (
+    <div className="h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
+      <Header />
+      <main className="flex-1 min-h-0">
+        <Routes>
+          {routes.map((route : IRoute) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default RouteLayout;
